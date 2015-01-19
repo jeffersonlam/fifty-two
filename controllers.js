b86e58310d40fb5785806f146236e8e1ce62063c
Jefferson Lam <jeffersonwlam@gmail.com>
2015-01-19 00:58:20 -0600

    added css. added screenshots. added more properties to each project

--- controllers.js
+++ controllers.js
@@ -9,11 +9,15 @@ fiftyTwoApp.controller('FiftyTwoCtrl', function($scope) {
     {'name': 'Weathery',
      'url': 'http://jeffersonlam.github.io/weathery/',
      'date': 'January 11th, 2015',
+     'git': 'https://github.com/jeffersonlam/weathery',
+     'img': 'img/weathery.jpg',
      'blurb': 'View the weather in gifs. This project was my exercise in raw Javascript for doing API requests in AJAX.'
- 	}, 
+    }, 
     {'name': 'Fifty Two',
      'url': 'http://jeffersonlam.github.io/fifty-two/',
      'date': 'January 19th, 2015',
+     'git': 'https://github.com/jeffersonlam/fifty-two',
+     'img': 'img/52.jpg',
      'blurb': 'A web page built in Angular to house my 52 week code project. The fact that you\'re looking at this means I was successful in building my first Angular app! One of my goals is to get comfortable with the MEAN stack (MongoDB, Express, Angular, Node), so learning Angular is my first step towards that goal.',
     }
   ];
