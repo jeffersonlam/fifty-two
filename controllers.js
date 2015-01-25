b4f1b346a37fed1ae5af9366bb4a28c984608b8a
JEFFERSON W. LAM <jwlam@us.ibm.com>
2015-01-25 17:40:51 -0600

    added messengerbot

--- controllers.js
+++ controllers.js
@@ -20,5 +20,12 @@ fiftyTwoApp.controller('FiftyTwoCtrl', function($scope) {
      'img': 'img/52.jpg',
      'blurb': 'A web page built in Angular to house my 52 week code project. The fact that you\'re looking at this means I was successful in building my first Angular app! One of my goals is to get comfortable with the MEAN stack (MongoDB, Express, Angular, Node), so learning Angular is my first step towards that goal.',
     }
+    {'name': 'Messengerbot',
+     'url': 'http://jeffersonlam.github.io/messengerbot/#/',
+     'date': 'January 25th, 2015',
+     'git': 'https://github.com/jeffersonlam/messengerbot',
+     'img': 'img/messengerbot.jpg',
+     'blurb': 'Messengerbot! I built this guy as an exercise in Angular and CSS animations. There\'s a lot of stuff I want to implement but I don\'t have the knowledge or time to do it. But, it\'s pretty cool as it is, and I\'m happy with it. Enjoy!'
+    }
   ];
 });
